454485cfb4d69f6fb1bd5f1a4aa3058df01a579f
Ashfaq Kadir Ovinno <akovinno.official@gmail.com>
2023-10-23 21:09:52 +0600

    subeen 2nd chapter started

---
+++ Tamim Shahriar Subeen C/1st part/2nd Chapter/program 2.3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a = 60, b = 50, sum;
+    sum = a + b;
+    printf("Sum is %d", sum);
+    return 0;
+}
---
+++ Tamim Shahriar Subeen C/1st part/2nd Chapter/type_cast.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = (int) 50.45;
+    printf("%d", a);
+    return 0;
+}
