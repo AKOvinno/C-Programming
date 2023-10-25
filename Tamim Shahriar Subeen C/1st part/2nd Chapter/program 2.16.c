ba40cd3a4de0c1e14b0c414a83a5a97437e62c12
Ashfaq Kadir Ovinno <akovinno.official@gmail.com>
2023-10-25 07:38:59 +0600

    Subeen 1st part chapter 2 completed

---
+++ Tamim Shahriar Subeen C/1st part/2nd Chapter/program 2.16.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    // test program = comment 1
+    printf("Hello ");
+    /* We have printed Hello. Now we shall print World.
+    Note that this is a multi-line comment */
+    printf("World\n"); // printed world
+
+    return 0;
+}
